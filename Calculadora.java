e4838254dc100067ba855a9dc4aaf1d44b79fc9c
olgasoteroescobar <osotesc2803@g.educaand.es>
2025-04-04 11:58:42 +0200

    Implementar método dividir en Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -3,4 +3,15 @@ public class Calculadora {
     public int suma(int a, int b) {
         return a + b;
     }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir entre cero");
+        }
+        return a / b;
+    }
 }
