ee62206f049e450182bcdbb074ec04c02d0702cf
olgasoteroescobar <osotesc2803@g.educaand.es>
2025-04-04 11:40:48 +0200

    Agregar clase pruebaCalculadora en la rama principal

---
+++ pruebaCalculadora.java
@@ -0,0 +1,8 @@
+public class pruebaCalculadora {
+        public static void main(String[] args) {
+            Calculadora calc = new Calculadora();
+            // Prueba el método suma
+            int resultado = calc.suma(5, 3);
+            System.out.println("Suma: " + resultado);
+        }
+}
